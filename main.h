b3c283a5818513fcb0d57c1f4d74add0ccd77498
sonofbossman <akingbadeeniola452@gmail.com>
2024-01-30 11:58:07 -0800

    A function that produces output according to a format.

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef main_h
+#define main_h
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+int _printf(const char *format, ...);
+int binary(int num);
+int octal(int num);
+#endif
